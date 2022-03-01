7eff06c2c3d468fbec611e31580e23898067e8ee
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-03-01 17:14:35 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,23 @@ const handleTouchMove = function (ev) {
 	ev.preventDefault();
 }
 
+const slideBanner = function () {
+	new Swiper('#slideBanner .swiper', {
+		speed: 1500,
+		slidesPerView: 1,
+		effect: 'fade',
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
+		},
+		navigation: {
+			nextEl: '#slideBanner .button-next',
+			prevEl: '#slideBanner .button-prev',
+		},
+	});
+}
+
 $(function () {
+	slideBanner();
 });
