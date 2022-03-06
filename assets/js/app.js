7a791b56d13af9f961facf9a21d7e87988eb2b7f
Jay B <79182784+JayBee-K@users.noreply.github.com>
2022-03-06 15:46:21 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,6 +120,25 @@ const selChuyenTien = function () {
     });
 }
 
+const selNhanTien = function () {
+    $('#sel-nhan').change(function () {
+        let elm = $(this);
+        let boxNhan = $('#content-nhan');
+        if (elm.val() != -1) {
+            boxNhan.find('#flag-nhan').attr('src', elm.find('option:selected').attr('data-flag'));
+            boxNhan.find('#name-nhan').html(elm.find('option:selected').attr('data-name'));
+            boxNhan.find('#value-code_nhan').html(elm.find('option:selected').attr('data-code'));
+            boxNhan.slideDown();
+        } else {
+            boxNhan.slideUp(function () {
+                boxNhan.find('#flag-nhan').attr('src', '');
+                boxNhan.find('#name-nhan').html('');
+                boxNhan.find('#value-code_nhan').html('');
+            });
+        }
+    });
+}
+
 const submitTinhThu = function () {
     $('#frmTinhThu').submit(function (e) {
         let selChuyenTien = $('#sel-chuyen');
@@ -129,10 +148,6 @@ const submitTinhThu = function () {
             alert('Vui lòng chọn tiền tệ và số tiền cần chuyển');
         } else {
             let boxNhan = $('#content-nhan');
-            let selNhan = $('#sel-nhan');
-            boxNhan.find('#flag-nhan').attr('src', selNhan.find('option:selected').attr('data-flag'));
-            boxNhan.find('#name-nhan').html(selNhan.find('option:selected').attr('data-name'));
-            boxNhan.find('#value-code_nhan').html(selNhan.find('option:selected').attr('data-code'));
             boxNhan.find('#preview-nhan').val('99999999');
             boxNhan.slideDown();
         }
@@ -143,11 +158,97 @@ const submitTinhThu = function () {
 const selTienTe = function () {
     $('#selTienTe').change(function () {
         let elm = $(this);
-        console.log(elm.val());
         if (elm.val() != -1) {
             $('#tiente').html(`Đơn vị: <b>${elm.find('option:selected').attr('data-code')}</b>`).addClass('active');
         } else {
             $('#tiente').removeClass('active').html('');
+        }
+    });
+}
+
+const selQuocGia = function () {
+    $('#selQuocGia').change(function () {
+        let elm = $(this);
+        let selNganHang = $('#selNganHang');
+        let optionFirst = `<option value="-1">Chọn ngân hàng</option>`;
+        if (elm.val() == -1) {
+            selNganHang.html(optionFirst);
+        } else {
+            switch (elm.val()) {
+                case "jp":
+                    optionFirst += `<option value="">Ngân hàng ở Nhật 01</option>
+                                    <option value="">Ngân hàng ở Nhật 02</option>
+                                    <option value="">Ngân hàng ở Nhật 03</option>
+                                    <option value="">Ngân hàng ở Nhật 04</option>
+                                    <option value="">Ngân hàng ở Nhật 05</option>
+                                    <option value="">Ngân hàng ở Nhật 06</option>
+                                    <option value="">Ngân hàng ở Nhật 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "vn":
+                    optionFirst += `<option value="">Ngân hàng ở Việt Nam 01</option>
+                                    <option value="">Ngân hàng ở Việt Nam 02</option>
+                                    <option value="">Ngân hàng ở Việt Nam 03</option>
+                                    <option value="">Ngân hàng ở Việt Nam 04</option>
+                                    <option value="">Ngân hàng ở Việt Nam 05</option>
+                                    <option value="">Ngân hàng ở Việt Nam 06</option>
+                                    <option value="">Ngân hàng ở Việt Nam 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "tw":
+                    optionFirst += `<option value="">Ngân hàng ở Đài Loan 01</option>
+                                    <option value="">Ngân hàng ở Đài Loan 02</option>
+                                    <option value="">Ngân hàng ở Đài Loan 03</option>
+                                    <option value="">Ngân hàng ở Đài Loan 04</option>
+                                    <option value="">Ngân hàng ở Đài Loan 05</option>
+                                    <option value="">Ngân hàng ở Đài Loan 06</option>
+                                    <option value="">Ngân hàng ở Đài Loan 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "kr":
+                    optionFirst += `<option value="">Ngân hàng ở Hàn Quốc 01</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 02</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 03</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 04</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 05</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 06</option>
+                                    <option value="">Ngân hàng ở Hàn Quốc 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "cn":
+                    optionFirst += `<option value="">Ngân hàng ở Trung Quốc 01</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 02</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 03</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 04</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 05</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 06</option>
+                                    <option value="">Ngân hàng ở Trung Quốc 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "sn":
+                    optionFirst += `<option value="">Ngân hàng ở Singapore 01</option>
+                                    <option value="">Ngân hàng ở Singapore 02</option>
+                                    <option value="">Ngân hàng ở Singapore 03</option>
+                                    <option value="">Ngân hàng ở Singapore 04</option>
+                                    <option value="">Ngân hàng ở Singapore 05</option>
+                                    <option value="">Ngân hàng ở Singapore 06</option>
+                                    <option value="">Ngân hàng ở Singapore 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                case "au":
+                    optionFirst += `<option value="">Ngân hàng ở Australia 01</option>
+                                    <option value="">Ngân hàng ở Australia 02</option>
+                                    <option value="">Ngân hàng ở Australia 03</option>
+                                    <option value="">Ngân hàng ở Australia 04</option>
+                                    <option value="">Ngân hàng ở Australia 05</option>
+                                    <option value="">Ngân hàng ở Australia 06</option>
+                                    <option value="">Ngân hàng ở Australia 07</option>`;
+                    selNganHang.html(optionFirst);
+                    break;
+                default:
+                    selNganHang.html(optionFirst);
+                    break;
+            }
         }
     });
 }
@@ -158,6 +259,8 @@ $(function () {
     clickOverlayMobile();
     slideBanner();
     selChuyenTien();
+    selNhanTien();
     submitTinhThu();
     selTienTe();
+    selQuocGia();
 });
